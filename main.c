36c29d74151468ae65bf2b9e8feb0ee96b45a8e4
smmurad <muradsigurd@gmail.com>
2021-12-04 14:00:42 +0100

    added blinky test function for debugging

--- main.c
+++ main.c
@@ -258,10 +258,34 @@ static void log_init(void) {
     NRF_LOG_DEFAULT_BACKENDS_INIT();
 } */
 
+static void led_toggle_task_function (void * pvParameter)
+{
+    UNUSED_PARAMETER(pvParameter);
+    while (true)
+    {
+        bsp_board_led_invert(BSP_BOARD_LED_0);
+
+        /* Delay a task for a given number of ticks */
+        vTaskDelay(100);
+
+        /* Tasks must be implemented to never return... */
+    }
+}
+
+TaskHandle_t  led_toggle_task_handle;   /**< Reference to LED0 toggling FreeRTOS task. */
+TimerHandle_t led_toggle_timer_handle;  /**< Reference to LED1 toggling FreeRTOS timer. */
+
+#define TASK_DELAY        200           /**< Task delay. Delays a LED0 task for 200 ms */
+#define TIMER_PERIOD      1000          /**< Timer period. LED1 timer will expire after 1000 ms */
+
 int main(void) {
     bsp_board_init(BSP_INIT_LEDS);
     clock_init();
     ir_init();
+
+    /* Create task for LED0 blinking with priority set to 2 */
+    if (true)
+        UNUSED_VARIABLE(xTaskCreate(led_toggle_task_function, "LED0", configMINIMAL_STACK_SIZE + 200, NULL, 2, &led_toggle_task_handle));
 
     
     position_estimate_t pos_est = {0,0,0};
