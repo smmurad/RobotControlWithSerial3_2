e836f71407e2a9671a67c7d4b65187041f2b9a32
smmurad <muradsigurd@gmail.com>
2021-12-06 11:06:20 +0100

    Added coap functionality to main DOESNT WORK YET

--- main.c
+++ main.c
@@ -32,9 +32,6 @@
 
 //Own files
 
-//Communication
-#include "arq.h"
-
 #include "network.h"
 #include "server_communication.h"
 #include "simple_protocol.h" 
@@ -73,6 +70,46 @@
 
 #include "robot_config.h"
 
+// Coap includes
+#include "app_scheduler.h"
+#include "app_timer.h"
+#include "bsp_thread.h"
+
+#include "thread_coap_utils.h"
+#include "thread_utils.h"
+
+#include <openthread/instance.h>
+#include <openthread/thread.h>
+
+#define THREAD_STACK_TASK_STACK_SIZE     (( 1024 * 8 ) / sizeof(StackType_t))   /**< FreeRTOS task stack size is determined in multiples of StackType_t. */
+#define LOG_TASK_STACK_SIZE              ( 1024 / sizeof(StackType_t))          /**< FreeRTOS task stack size is determined in multiples of StackType_t. */
+#define THREAD_STACK_TASK_PRIORITY       2
+#define LOG_TASK_PRIORITY                1
+#define LED1_TASK_PRIORITY               1
+#define LED2_TASK_PRIORITY               1
+#define LED1_BLINK_INTERVAL              427
+#define LED2_BLINK_INTERVAL              472
+#define SCHED_QUEUE_SIZE                 32                                      /**< Maximum number of events in the scheduler queue. */
+#define SCHED_EVENT_DATA_SIZE            APP_TIMER_SCHED_EVENT_DATA_SIZE        
+
+typedef struct
+{
+    TaskHandle_t     thread_stack_task;     /**< Thread stack task handle */
+    TaskHandle_t     logger_task;           /**< Definition of Logger task. */
+    TaskHandle_t     led1_task;             /**< LED1 task handle*/
+    TaskHandle_t     led2_task;             /**< LED2 task handle*/
+    
+} application_t;
+
+
+application_t m_app =
+{
+    .thread_stack_task = NULL,
+    .logger_task       = NULL,
+    .led1_task         = NULL,
+    .led2_task         = NULL,
+    .led2_task         = NULL,
+};
 
 #define NRF_LOG_ENABLED 1
 
@@ -95,6 +132,7 @@ static void logger_thread(void * arg)
     {
         NRF_LOG_FLUSH();
 
+        // Deactivated this as the thread did not activate itself - Sigurd
         // vTaskSuspend(NULL); // Suspend myself
     }
 }
@@ -126,13 +164,10 @@ SemaphoreHandle_t mutex_spi;
 SemaphoreHandle_t mutex_i2c;
 //SemaphoreHandle_t xCollisionMutex;
 
-
 /* Queues */
 QueueHandle_t poseControllerQ = 0;
 QueueHandle_t scanStatusQ = 0;
 QueueHandle_t queue_microsd = 0;
-
-
 
 // Flag to indicate connection status. Interrupt can change handshook status
 uint8_t gHandshook = false;
@@ -140,6 +175,230 @@ uint8_t gPaused = false;
 
 // all SPI driver interaction occurs within mutex, so can safely use global bool
 bool shared_SPI_init = false;
+
+/////////////////// ADDDING COAP CODE ////////////////////
+
+static inline void light_on(void)
+{
+    vTaskResume(m_app.led1_task);
+    vTaskResume(m_app.led2_task);
+}
+
+
+static inline void light_off(void)
+{
+    vTaskSuspend(m_app.led1_task);
+    LEDS_OFF(BSP_LED_2_MASK);
+
+    vTaskSuspend(m_app.led2_task);
+    LEDS_OFF(BSP_LED_3_MASK);
+}
+
+
+static inline void light_toggle(void)
+{
+    if (!thread_coap_utils_light_is_led_blinking())
+    {
+        light_on();
+    }
+    else
+    {
+        light_off();
+    }
+}
+
+
+static void on_light_change(thread_coap_utils_light_command_t command)
+{
+    switch (command)
+    {
+        case THREAD_COAP_UTILS_LIGHT_CMD_ON:
+            light_on();
+            break;
+
+        case THREAD_COAP_UTILS_LIGHT_CMD_OFF:
+            light_off();
+            break;
+
+        case THREAD_COAP_UTILS_LIGHT_CMD_TOGGLE:
+            light_toggle();
+            break;
+
+        default:
+            ASSERT(false);
+            break;
+    }
+}
+
+/***************************************************************************************************
+ * @section Signal handling
+ **************************************************************************************************/
+
+void otTaskletsSignalPending(otInstance * p_instance)
+{
+    if (m_app.thread_stack_task == NULL)
+    {
+        return;
+    }
+
+    UNUSED_RETURN_VALUE(xTaskNotifyGive(m_app.thread_stack_task));
+}
+
+
+void otSysEventSignalPending(void)
+{
+    static BaseType_t xHigherPriorityTaskWoken;
+
+    if (m_app.thread_stack_task == NULL)
+    {
+        return;
+    }
+
+    vTaskNotifyGiveFromISR(m_app.thread_stack_task, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
+/***************************************************************************************************
+ * @section State change handling
+ **************************************************************************************************/
+
+ static void thread_state_changed_callback(uint32_t flags, void * p_context)
+{
+    if (flags & OT_CHANGED_THREAD_ROLE)
+    {
+        switch(otThreadGetDeviceRole(p_context))
+        {
+            case OT_DEVICE_ROLE_CHILD:
+            case OT_DEVICE_ROLE_ROUTER:
+            case OT_DEVICE_ROLE_LEADER:
+                break;
+
+            case OT_DEVICE_ROLE_DISABLED:
+            case OT_DEVICE_ROLE_DETACHED:
+            default:
+                thread_coap_utils_provisioning_enable_set(false);
+                break;
+        }
+    }
+
+    NRF_LOG_INFO("State changed! Flags: 0x%08x Current role: %d\r\n", flags, otThreadGetDeviceRole(p_context));
+}
+
+/***************************************************************************************************
+ * @section Buttons
+ **************************************************************************************************/
+
+static void bsp_event_handler(bsp_event_t event)
+{
+    switch (event)
+    {
+        case BSP_EVENT_KEY_3:
+            thread_coap_utils_provisioning_enable_set(true);
+            break;
+
+        default:
+            return;
+    }
+}
+
+/***************************************************************************************************
+ * @section Initialization
+ **************************************************************************************************/
+
+ /**@brief Function for initializing the Application Timer Module
+ */
+static void timer_init(void)
+{
+    uint32_t error_code = app_timer_init();
+    APP_ERROR_CHECK(error_code);
+}
+
+
+/**@brief Function for initializing the Thread Board Support Package
+ */
+static void thread_bsp_init(void)
+{
+    if(false)
+    {
+        uint32_t error_code = bsp_init(BSP_INIT_LEDS | BSP_INIT_BUTTONS, bsp_event_handler);
+        APP_ERROR_CHECK(error_code);
+    }
+
+    uint32_t error_code1 = bsp_thread_init(thread_ot_instance_get());
+    APP_ERROR_CHECK(error_code1);
+}
+
+
+/**@brief Function for initializing the Thread Stack
+ */
+static void thread_instance_init(void)
+{
+    thread_configuration_t thread_configuration =
+    {
+        .radio_mode        = THREAD_RADIO_MODE_RX_ON_WHEN_IDLE,
+        .autocommissioning = true,
+    };
+
+    thread_init(&thread_configuration);
+    thread_cli_init();
+    thread_state_changed_callback_set(thread_state_changed_callback);
+}
+
+
+/**@brief Function for initializing the Constrained Application Protocol Module
+ */
+static void thread_coap_init(void)
+{
+    thread_coap_utils_configuration_t thread_coap_configuration =
+    {
+        .coap_server_enabled               = true,
+        .coap_client_enabled               = false,
+        .configurable_led_blinking_enabled = true,
+    };
+
+    thread_coap_utils_init(&thread_coap_configuration);
+}
+
+static void thread_stack_task(void * arg)
+{
+    UNUSED_PARAMETER(arg);
+
+    while (1)
+    {
+        thread_process();
+        UNUSED_RETURN_VALUE(ulTaskNotifyTake(pdTRUE, portMAX_DELAY));
+    }
+}
+
+/***************************************************************************************************
+ * @section Leds
+ **************************************************************************************************/
+
+static void led1_task(void * arg)
+{
+    UNUSED_PARAMETER(arg);
+
+    while (1)
+    {
+        LEDS_INVERT(BSP_LED_2_MASK);
+        vTaskDelay(LED1_BLINK_INTERVAL);
+    }
+}
+
+
+static void led2_task(void * arg)
+{
+    UNUSED_PARAMETER(arg);
+
+    while (1)
+    {
+        LEDS_INVERT(BSP_LED_3_MASK);
+        vTaskDelay(LED2_BLINK_INTERVAL);
+    }
+}
+
+/////////////////// COAP END ////////////////////
+
 
 /**@brief A function which is hooked to idle task.
  * @note Idle hook must be enabled in FreeRTOS configuration (configUSE_IDLE_HOOK).
@@ -257,7 +516,7 @@ static void led_toggle_task_function (void * pvParameter)
         bsp_board_led_invert(BSP_BOARD_LED_0);
 
         /* Delay a task for a given number of ticks */
-        vTaskDelay(10000);
+        vTaskDelay(1000);
 
         /* Tasks must be implemented to never return... */
     }
@@ -275,8 +534,39 @@ int main(void) {
     ir_init();
     log_init();
 
+    // ADDING COAP INIT //
+    #ifdef MBEDTLS_THREADING
+    freertos_mbedtls_mutex_init();
+    #endif
+    timer_init();
+    thread_instance_init();
+    thread_coap_init();
+    thread_bsp_init();
+    thread_coap_utils_light_command_handler_set(on_light_change);
+
+    if(false)
+    {
+        //COAP init end//
+        // Start thread stack execution.
+        if (pdPASS != xTaskCreate(thread_stack_task, "THR", THREAD_STACK_TASK_STACK_SIZE, NULL, 2, &m_app.thread_stack_task))
+        {
+            APP_ERROR_HANDLER(NRF_ERROR_NO_MEM);
+        }
+    }
+    // Start execution.
+    if (pdPASS != xTaskCreate(led1_task, "LED1", configMINIMAL_STACK_SIZE, NULL, 1, &m_app.led1_task))
+    {
+        APP_ERROR_HANDLER(NRF_ERROR_NO_MEM);
+    }
+
+    // Start execution.
+    if (pdPASS != xTaskCreate(led2_task, "LED2", configMINIMAL_STACK_SIZE, NULL, 1, &m_app.led2_task))
+    {
+        APP_ERROR_HANDLER(NRF_ERROR_NO_MEM);
+    }
+
     /* Create task for LED0 blinking with priority set to 2 */
-    if (true)
+    if (false)
         UNUSED_VARIABLE(xTaskCreate(led_toggle_task_function, "LED0", configMINIMAL_STACK_SIZE + 200, NULL, 2, &led_toggle_task_handle));
 
     
@@ -312,9 +602,6 @@ int main(void) {
    //error_msg = xTraceGetLastError();
 #endif
 
-    // Do not start any interrupt that uses system functions before system initialisation.
-    // The best solution is to start the OS before any other initalisation.
-    arq_init();
 
     #if NRF_LOG_ENABLED
 		if (pdPASS != xTaskCreate(logger_thread, "LOGGER", 256, NULL, 1, &m_logger_thread))
@@ -330,7 +617,6 @@ int main(void) {
     encoderTicksToMotorPositionControllerQ = xQueueCreate(100, sizeof(encoderTicks)); 
     encoderTicksToEstimatorTaskQ = xQueueCreate(100, sizeof(encoderTicks)); 
 
-
 	//initialize mutexes
 	mutex_spi = xSemaphoreCreateMutex();
     mutex_i2c = xSemaphoreCreateMutex();
@@ -340,11 +626,10 @@ int main(void) {
 	xControllerBSem = xSemaphoreCreateBinary(); // Estimator to Controller synchronization
 	xCommandReadyBSem = xSemaphoreCreateBinary();
 	
-	
+    // Disable because it crashed the program -Sigurd 
     // if (pdPASS != xTaskCreate(display_task, "DISP", 128, NULL, 1, &handle_display_task))
     //     APP_ERROR_HANDLER(NRF_ERROR_NO_MEM); 
 
-    
     if (pdPASS != xTaskCreate(user_task, "USER", 128, NULL, 4, &handle_user_task)) //needs elevated priority because init functions
         APP_ERROR_HANDLER(NRF_ERROR_NO_MEM);
     
@@ -358,6 +643,7 @@ int main(void) {
         APP_ERROR_HANDLER(NRF_ERROR_NO_MEM);
     }
     */
+   
     /*
     if (pdPASS != xTaskCreate(Encoder_tester, "Encoder Test", 256, NULL, 1, &encoder_tester)){
         APP_ERROR_HANDLER(NRF_ERROR_NO_MEM);
